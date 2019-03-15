49beee63eaccfbf927fa25aebfde75ac4c6ec7c6
HarryMarshandi <44866511+HarryMarshandi@users.noreply.github.com>
2019-03-15 10:36:23 +0700

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="/2019/03/so-many-affiliate-programs-which-one-do.html"}
 if(x=="2"){xxx="/2019/03/5-qualities-for-affiliate-marketing.html"}
 if(x=="3"){xxx="/2019/03/getting-started-in-advertising-most-of.html"}
 if(x=="4"){xxx="/2019/03/advertising-through-video-games.html"}
-if(x=="5"){xxx="/2019/03/5-easy-ways-to-get-your-creative-juices-going.html"}a_to_ve[i].href="https://tungguajaaa.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2019/03/5-easy-ways-to-get-your-creative-juices-going.html"}a_to_ve[i].href="https://tungguajaaa.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
